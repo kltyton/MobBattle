fdda54320de227fc1a2416ceb52b9015502d840c
kltyton <h3193778245@163.com>
2026-05-29 18:38:03 +0800

    init

---
+++ src/main/java/com/kltyton/mob_battle/client/Mob_battleClient.java
@@ -0,0 +1,10 @@
+package com.kltyton.mob_battle.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Mob_battleClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
